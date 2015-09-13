9522d5f2a909c30c7cace2eb33677b8bc03f1a25
hussard bleu <bleuhussard@gmail.com>
2015-09-13 21:08:55 +0200

    plot1 add data import / unzip

--- plot1.R
+++ plot1.R
@@ -1,5 +1,11 @@
 
 library(data.table)
+if (!file.exists("data.zip")) {
+    download.file(url="https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",
+                  destfile="data.zip",
+                  method="curl")
+    unzip("data.zip")  
+}
 dat <- fread("household_power_consumption.txt")
 dat$Date<-as.Date(dat$Date,format="%d/%m/%Y")
 dat_sub <- dat[Date>="2007-02-01" & Date<="2007-02-02"]
