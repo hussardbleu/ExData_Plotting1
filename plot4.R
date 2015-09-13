8b1e71213c6199077c4b350b762310a3f681b0d3
hussard bleu <bleuhussard@gmail.com>
2015-09-13 22:39:16 +0200

    script and graph for plot4

--- plot4.R
+++ plot4.R
@@ -32,19 +32,5 @@ with(dat_sub, {
          col="black", xlab="datetime", ylab="Global_reactive_power")
 })
 
-
-plot(dat_sub$Date_Time, 
-     dat_sub$Sub_metering_1, 
-     type="l",
-     col="black", 
-     xlab="", 
-     ylab="Energy sub metering", 
-     main="")
-lines(dat_sub$Date_Time, dat_sub$Sub_metering_2, col="red")
-lines(dat_sub$Date_Time, dat_sub$Sub_metering_3, col="blue")
-legend("topright", 
-       col = c("black","red","blue"), 
-       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
-       lwd=1, lty=1)
 dev.copy(png, file = "plot4.png", width=480, height=480) 
 dev.off()
